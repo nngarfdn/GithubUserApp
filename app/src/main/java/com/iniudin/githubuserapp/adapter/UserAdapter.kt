863776956b225fd6f0bab8dccba29ad3b8bd575e
nngarfdn <nanangarif404@gmail.com>
2020-07-17 13:42:13 +0700

    UI/UX dan API

--- app/src/main/java/com/iniudin/githubuserapp/adapter/UserAdapter.kt
+++ app/src/main/java/com/iniudin/githubuserapp/adapter/UserAdapter.kt
@@ -3,15 +3,31 @@ package com.iniudin.githubuserapp.adapter
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.iniudin.githubuserapp.R
-import com.iniudin.githubuserapp.activity.DetailUserActivity
-import com.iniudin.githubuserapp.model.User
+import com.iniudin.githubuserapp.ui.activity.DetailUserActivity
+import com.iniudin.githubuserapp.pojo.ItemsGithubSearch
+import kotlinx.android.synthetic.main.user_item.view.*
 
-class UserAdapter(private val cardList: MutableList<User>, val context: Context) :
-    RecyclerView.Adapter<UserViewHolder>() {
+class UserAdapter( val context: Context) :
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    private lateinit var cardList: List<ItemsGithubSearch>
+
+    fun setData(cardList: List<ItemsGithubSearch>){
+        this.cardList = cardList
+    }
+
+    inner class UserViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val name = v.txtName!!
+        val location = v.txtLocation!!
+        val photo: ImageView = v.imgPhoto
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
@@ -25,20 +41,17 @@ class UserAdapter(private val cardList: MutableList<User>, val context: Context)
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val currentItem = cardList[position]
 
-        holder.name.text = currentItem.name
-        holder.location.text = currentItem.location
+        holder.name.text = currentItem.login
+        holder.location.text = currentItem.id.toString()
 
         Glide.with(context)
-            .load(context.resources.getIdentifier(currentItem.avatar, "drawable", context.packageName))
+            .load(currentItem.avatar_url)
             .into(holder.photo)
 
         holder.itemView.setOnClickListener {
             val intent = Intent(context, DetailUserActivity::class.java)
-            intent.putExtra(DetailUserActivity.EXTRA_USER, currentItem)
+            intent.putExtra(DetailUserActivity.EXTRA_USER, currentItem.login)
             context.startActivity(intent)
         }
-
     }
-
-
 }
